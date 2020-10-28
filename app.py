8c33ff2efa344b498c29c2bdd28cf9fe251e694c
tomivanpete <tmimp916@gmail.com>
2020-10-27 21:50:29 -0700

    added todo lists

--- app.py
+++ app.py
@@ -10,11 +10,21 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
+    def __repr__(self):
+        return f'<List: {self.id} {self.name}>'
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo: {self.id} {self.description}>'
@@ -25,11 +35,35 @@ def create_todo():
     body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description=description)
+        list_id = request.get_json()['list_id']
+        todo = Todo(description=description, list_id=list_id)
         db.session.add(todo)
         db.session.commit()
         body['description'] = todo.description
         body['id'] = todo.id
+        body['list_id'] = todo.list_id
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify(body)
+
+@app.route('/lists/create', methods=['POST'])
+def create_todo_list():
+    error = False
+    body = {}
+    try:
+        name = request.get_json()['name']
+        todo_list = TodoList(name=name)
+        db.session.add(todo_list)
+        db.session.commit()
+        body['name'] = todo_list.name
+        body['id'] = todo_list.id
     except:
         error = True
         db.session.rollback()
@@ -79,6 +113,13 @@ def delete_todo(todo_id):
     else:
         return jsonify(success=True)
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+    lists=TodoList.query.order_by('id').all(),
+    active_list=TodoList.query.get(list_id),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', todos=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
